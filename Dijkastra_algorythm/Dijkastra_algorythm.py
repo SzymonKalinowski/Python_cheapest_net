eb24934df79bb00d446037040fb8d0539fb7188f
Szymon <u122_szykal_waw@technischools.com>
2025-06-02 19:02:27 +0200

    poprawiona dijkstra

--- Dijkastra_algorythm/Dijkastra_algorythm.py
+++ Dijkastra_algorythm/Dijkastra_algorythm.py
@@ -1,30 +1,53 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class Dijkstra:
     graph: dict
     start: str
     end: str = None
+    distances: dict = field(init=False, default_factory=dict)
+    previous_nodes: dict = field(init=False, default_factory=dict)
 
-    def Dijkastra_algorythm(self):
-        current_node = self.start
-        all_c = []
-        distances: int
-        if self.start not in self.graph.keys():
-            raise ValueError("Start node is not in graph")
-        else:
-            for i in self.graph.keys():
-                if self.start == i:
-                    current_node = i
-                    distances = 0
-                    all_c = [current_node]
-                    self.graph[current_node][current_node] = 0
-                    while current_node != self.end:
-                        min_dist = float("inf")
-                        for node in self.graph.keys():
-                            if self.graph[current_node][node] < min_dist and node not in all_c:
-                                min_dist = self.graph[current_node][node]
-                                next_node = node
-                        all_c.append(next_node)
-                        current_node = next_node
+    def dijkstra_algorithm(self):
+        if self.start not in self.graph:
+            raise ValueError("Start node is not in the graph")
 
+        self.distances = {node: float('inf') for node in self.graph}
+        self.distances[self.start] = 0
+        self.previous_nodes = {node: None for node in self.graph}
+        visited = set()
+
+        while len(visited) < len(self.graph):
+            current_node = None
+            current_min_distance = float('inf')
+            for node in self.graph:
+                if node not in visited and self.distances[node] < current_min_distance:
+                    current_min_distance = self.distances[node]
+                    current_node = node
+
+            if current_node is None:
+                break
+
+            visited.add(current_node)
+
+            for neighbor, weight in self.graph[current_node].items():
+                if neighbor not in visited:
+                    new_distance = self.distances[current_node] + weight
+                    if new_distance < self.distances[neighbor]:
+                        self.distances[neighbor] = new_distance
+                        self.previous_nodes[neighbor] = current_node
+
+        if self.end:
+            return self.get_path(), self.distances[self.end]
+        return self.distances
+
+    def get_path(self):
+        if self.end not in self.distances or self.distances[self.end] == float('inf'):
+            return None
+
+        path = []
+        current = self.end
+        while current is not None:
+            path.insert(0, current)
+            current = self.previous_nodes[current]
+        return path
