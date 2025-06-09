fe954663239c59a3c84810c1cbb901bef9ac791a
SzymonKalinowski <146426686+SzymonKalinowski@users.noreply.github.com>
2025-06-09 12:50:36 +0200

    dupa

--- give_and_take_energy/give_and_take_energy.py
+++ give_and_take_energy/give_and_take_energy.py
@@ -6,9 +6,16 @@ class GiveAndTakeEnergy:
     data: list
     city_take: str
     city_give: str
-    take_from: float
-    give_to: take_from
+    how_much: float
 
     def GiveAndTakeEnergy(self):
+        if self.city_take == self.city_give:
+            raise ValueError("Cities cannot be the same")
+        if self.city_take not in self.data:
+            raise ValueError(f"{self.city_take} doesn`t exist")
+        if self.city_give not in self.data:
+            raise ValueError(f"{self.city_give} doesn`t exist")
         for city in self.data:
-            pass
+            if
+
+
